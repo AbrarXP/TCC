0a9e7bed64025ad3c60eaad3642f7c7e7eff5dda
Abrar <132360640+AbrarXP@users.noreply.github.com>
2025-03-23 09:54:24 +0700

    Update Database.js

--- BE/config/Database.js
+++ BE/config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
-const db = new Sequelize("note_db", "root", "", {
-  host: "localhost",
+const db = new Sequelize("note_db", "admin", "bandengpresto", {
+  host: "34.172.55.64",
   dialect: "mysql",
 });
 
