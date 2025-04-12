fcb2151c8ca1806c5f5e9293d04c129572886fcd
Abrar <132360640+AbrarXP@users.noreply.github.com>
2025-04-12 16:12:27 +0700

    Update index.js

--- BE/index.js
+++ BE/index.js
@@ -2,13 +2,10 @@ import express from "express";
 import cors from "cors";
 import NoteRoute from "./routes/NoteRoute.js";
 
-console.log("PORT dari ENV:", process.env.PORT);
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(NoteRoute);
 
-app.listen(PORT, () => console.log("Server connected ${PORT}"));
+app.listen(5000, () => console.log("Server connected"));
