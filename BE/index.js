eea26d1e6e7c45b21ba51060dc26b961a11767ca
AbrarXP <muhammadabrarkuliah@gmail.com>
2025-05-20 20:24:56 +0700

    cookies setting

--- BE/index.js
+++ BE/index.js
@@ -4,12 +4,20 @@ import NoteRoute from "./routes/NoteRoute.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://fe-dot-hazel-airlock-451115-h0.et.r.appspot.com",  // domain FE kamu
+  credentials: true
+}));
 app.use(express.json());
 app.use(NoteRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello from backend!");
+  res.cookie("token", token, {
+    httpOnly: false,
+    sameSite: "None",   // ⬅️ WAJIB
+    secure: true        // ⬅️ WAJIB jika SameSite=None
+    });
 });
 
 const PORT = 5000;
